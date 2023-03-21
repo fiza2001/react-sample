aa9fcbb84a4217f903f76f6196f9ee18d66dbc46
Fiza Hussain <fiza16hussain@gmail.com>
2023-03-21 19:08:00 +0530

    added another h1 and styles

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
       <h1>Hi!!! I'm Fiza Hussain</h1>
       <br /><br />
       <h1>The React is working !!!!!</h1>
+      <br /><br />
+      <h1>Finally its working</h1>
     </div>
   );
 }
